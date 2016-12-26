47d0c082ee20f18cd791240455f6cf0a518b4d50
Miguel Atilio Godoy <miguel@10x.co>
2016-12-26 18:37:42 -0300

    fix Api with not found

--- BananaTicket/app/controllers/validation_controller.rb
+++ BananaTicket/app/controllers/validation_controller.rb
@@ -2,7 +2,11 @@ class ValidationController < ApplicationController
   def index
     ticket = Ticket.find_by(code: params[:code])
     respond_to do |format|
-      format.json { render json: ticket.event.name}
+      if ticket
+        format.json { render json: ticket.event.name}
+      else
+        raise ActionController::RoutingError.new('Not Found')
+      end
     end
   end
 end
