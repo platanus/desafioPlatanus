b383b2a20d80776e3d9e0cbdfc47116507d83feb
Miguel Atilio Godoy <miguel@10x.co>
2016-12-26 16:07:39 -0300

    Fix event

--- BananaTicket/db/migrate/20161226184817_create_events.rb
+++ BananaTicket/db/migrate/20161226184817_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
       t.string :name
-      t.datetime :started_at
+      t.datetime :starts_at
 
       t.timestamps
     end
